c968ada32d49dc74a141c3ae783ee36607f8eb43
iburnthings <iburnthings@hotmail.com>
2022-07-14 00:31:13 -0600

    Intro to built-in functions and methods

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 from tkinter import messagebox
+from turtle import title
 
 
 message = "Hello World!"
@@ -6,3 +7,7 @@ print(message)
 
 message = "New Message"
 print(message)
+
+message = "monty python's flying circus"
+print(message)
+print(message.title())

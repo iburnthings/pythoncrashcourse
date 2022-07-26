92567f6d242bd660438542275a2f9aa52ee6453e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 15:08:01 -0600

    sync

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,10 @@
-from tkinter import messagebox
-from turtle import title
-
-
 message = "Hello World!"
 print(message)
 
 message = "New Message"
 print(message)
 
+#String methods
 message = "monty python's flying circus"
 print(message)
 print(message.title())
